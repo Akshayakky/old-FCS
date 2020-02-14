ea293aa8e8215a3fe04e3f9f902de92f142ea6c9
akshayakky <akshaysmpt01@gmail.com>
2020-02-14 18:50:43 +0530

    ADDED : Flip Two Coins And Find Percent Of Doublet Combination Using Dictionary

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -2,24 +2,42 @@
 
 #CONSTANTS
 IS_HEAD=0
+DOUBLET=2
 
 #DECLARE DICTIONARY
-declare -A singletDictionary
+declare -A doubletDictionary
 
 #READING VALUE FOR NO OF ITERATIONS
 read -p "Enter No of Iterations : " numberIteration
 
-for (( index=1; index<=$numberIteration; index++ ))
-do
-	randomFlip=$((RANDOM%2))
-	#STORING HEADCOUNT AND TAILCOUNT IN DICTIONARY
-	if [ $randomFlip -eq $IS_HEAD ]
-	then
-		((singletDictionary[HEADS]++))
-	else
-		((singletDictionary[TAILS]++))
-	fi
-done
+#FUNCTION TO SIMULATE DOUBLET COMBINATION
+function flipCoin(){
+	for (( index=1; index<=$1; index++ ))
+	do
+		for (( coins=1; coins<=$2; coins++ ))
+		do
+			randomFlip=$((RANDOM%2))
+			if [ $randomFlip -eq $IS_HEAD ]
+			then
+				flipCombination=$flipCombination"H"
+			else
+				flipCombination=$flipCombination"T"
+			fi
+		done
+		#STORING HEAD AND TAIL COMBINATION COUNT IN DICTIONARY
+		((doubletDictionary[$flipCombination]++))
+		flipCombination=""
+	done
+}
 
-percentHeads=`echo "scale=2; ${singletDictionary[HEADS]}*100/$numberIteration" | bc`
-percentTails=`echo "scale=2; ${singletDictionary[TAILS]}*100/$numberIteration" | bc`
+#FUNCTION TO STORE COIN FLIP COMBINATION AND CORRESPONDING PERCENTAGE
+function calculatePercentage(){
+	for i in ${!doubletDictionary[@]}
+	do
+#		doubletDictionary[$i]=`echo "scale=2; ${doubletDictionary[$i]}*100/$numberIteration" | bc`
+		doubletDictionary[$i]=`echo "scale=2; ${doubletDictionary[$i]}*100/$numberIteration" | bc | awk '{printf "%.3f",$0}'`
+	done
+}
+
+flipCoin $numberIteration $DOUBLET
+calculatePercentage
