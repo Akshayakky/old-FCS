7ef8fea6db4db6ebdbe797a1c90b55cdc4d7dc97
akshayakky <akshaysmpt01@gmail.com>
2020-02-15 23:47:02 -0500

    ADDED : Sorting All Combination Dictionaries And Finding Winning Combination In Each Dictionary

--- FlipCoinSimulator.sh
+++ FlipCoinSimulator.sh
@@ -31,10 +31,10 @@ function flipCoin(){
 		done
 
 		#STORING HEAD AND TAIL COMBINATION COUNT IN CORRESPONDING DICTIONARY
-		if [ $NO_OF_COINS -eq 1 ]
+		if [ $NO_OF_COINS -eq $SINGLET ]
 		then
 			((singletDictionary[$flipCombination]++))
-		elif [ $NO_OF_COINS -eq 2 ]
+		elif [ $NO_OF_COINS -eq $DOUBLET ]
 		then
 			((doubletDictionary[$flipCombination]++))
 		else
@@ -44,16 +44,16 @@ function flipCoin(){
 	done
 }
 
-#FUNCTION TO STORE COIN FLIP COMBINATION AND CORRESPONDING PERCENTAGE
+#CONVERTING COUNT TO PERCENTAGE IN ALL COMBINATION DICTIONARIES
 function calculatePercentage(){
 	local NO_OF_COINS=$1
-	if [ $NO_OF_COINS -eq 1 ]
+	if [ $NO_OF_COINS -eq $SINGLET ]
 	then
 		for i in ${!singletDictionary[@]}
 		do
 			singletDictionary[$i]=`echo "scale=2; ${singletDictionary[$i]}*100/$numberIteration" | bc`
 		done
-	elif [ $NO_OF_COINS -eq 2 ]
+	elif [ $NO_OF_COINS -eq $DOUBLET ]
 	then
 		for i in ${!doubletDictionary[@]}
 		do
@@ -67,6 +67,29 @@ function calculatePercentage(){
 	fi
 }
 
+#FUNCTION TO SORT COMBINATIONS PERCENTAGE WISE IN DESCENDING ORDER
+function sort(){
+	keysAndValuesArray=("$@")
+	local count=1
+	local NO_OF_RECORDS=$((${#keysAndValuesArray[@]}/2))
+	#USING BUBBLESORT TO SORT KEYS AND VALUES IN keysAndValuesArray ARRAY
+	for (( i=0; i<$NO_OF_RECORDS; i++ ))
+	do
+		for (( j=0; j<$(($NO_OF_RECORDS-1)); j++ ))
+		do
+			if (( $(echo "${keysAndValuesArray[j+$NO_OF_RECORDS]} < ${keysAndValuesArray[j+$NO_OF_RECORDS+1]}" |bc -l) ))
+			then
+				temp=${keysAndValuesArray[j]}
+				keysAndValuesArray[j]=${keysAndValuesArray[j+1]}
+				keysAndValuesArray[j+1]=$temp
+				temp=${keysAndValuesArray[j+$NO_OF_RECORDS]}
+				keysAndValuesArray[j+$NO_OF_RECORDS]=${keysAndValuesArray[j+1+$NO_OF_RECORDS]}
+				keysAndValuesArray[j+1+$NO_OF_RECORDS]=$temp
+			fi
+		done
+	done
+}
+
 flipCoin $numberIteration $SINGLET
 flipCoin $numberIteration $DOUBLET
 flipCoin $numberIteration $TRIPLET
@@ -74,3 +97,12 @@ flipCoin $numberIteration $TRIPLET
 calculatePercentage $SINGLET
 calculatePercentage $DOUBLET
 calculatePercentage $TRIPLET
+
+sort ${!singletDictionary[@]} ${singletDictionary[@]}
+singletWinningCombination=${keysAndValuesArray[0]}
+
+sort ${!doubletDictionary[@]} ${doubletDictionary[@]}
+doubletWinningCombination=${keysAndValuesArray[0]}
+
+sort ${!tripletDictionary[@]} ${tripletDictionary[@]}
+tripletWinningCombination=${keysAndValuesArray[0]}
